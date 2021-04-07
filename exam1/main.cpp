1310b9656f164814723a3c6a80aed609c141a8b5
currylien <lianmelody102321@gmail.com>
2021-04-07 02:39:15 -0700

    add main.cpp

--- exam1/main.cpp
+++ exam1/main.cpp
@@ -11,40 +11,40 @@ AnalogOut Aout(D7);
 AnalogIn Ain(A0);
 
 float frequency[4] = {0.125, 0.25, 0.5, 1};
+int wait[4] = {220, 200, 160,80};
+int test[4] = {230, 220, 200, 160};
 int f = 0;
 float frequency_use = 0.125;
 Thread t;
+
 int sample = 128;
 float ADCdata[256];
+
+void print_data() {
+    for (int i = 0; i < 128; i++){
+        printf("%f\r\n", ADCdata[i]);
+        ThisThread::sleep_for(10ms);
+    }  
+}
 
 void ISR1()
 {
     if (f <= 2) {
             f++;
-            //uLCD.locate(0,(f-1));
-            //uLCD.printf("  %f Hz", frequency[f - 1]);
-            //uLCD.locate(0,f);
-            //uLCD.printf("* %f Hz", frequency[f]);
-            //frequency_use = frequency[f];
     }
 }
 
 void ISR2() {
     if (f >= 1) {
             f--;
-            //uLCD.locate(0, (f + 1));
-            //uLCD.printf("  %f Hz", frequency[f + 1]);
-            //uLCD.locate(0, f);
-            //uLCD.printf("* %f Hz", frequency[f]);
-            //frequency_use = frequency[f];
     }
 }
 void wave_generate() {
-    while(1){
     int k = 0;
     float gap1, gap2;
-    gap1 = (1.0f  / (sample * 0.333f * frequency[f])) ;//4.166f;
-    gap2 = (1.0f / (sample * 0.333f * frequency[f]));//4.166f;
+    int g = 1 - (2.0f / 3.0f) *frequency[f];
+    gap1 = (1.0f  / (sample * 0.333f * frequency[f])) *4.166f;
+    gap2 = (1.0f / (sample * 0.333f * frequency[f]))*4.166f;
     for (float j = 0; j <= 1; j = j + gap1) {
             Aout = j;
             if (k < sample) {
@@ -53,14 +53,14 @@ void wave_generate() {
             }
             ThisThread::sleep_for(1000ms / sample);
         }
-    for (float w = 0; w <= (sample - sample * (2.0f / 3) * frequency[f]); w+=1) {
+    //for (float w = 0; w <= (sample - sample * (2.0f / 3) * frequency[f]); w+=1) {
             Aout = 1;
-            if (k < sample) {
-                ADCdata[k] = Ain;
+            while (k < (sample *  (test[f] / 240))){
+                ADCdata[k] = 1;
                 k++;
             }
-            ThisThread::sleep_for(1000ms / sample);
-        }
+            ThisThread::sleep_for(wait[f] * 1ms);
+        //}
     for (float j = 1; j >= 0; j = j - gap2) {
             Aout = j;
             if (k < sample) {
@@ -69,7 +69,7 @@ void wave_generate() {
             }
             ThisThread::sleep_for(1000ms / sample);
         }
-    }
+    queue.call(print_data);
 }
 int main()
 {
